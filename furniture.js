40009316bb0387586bf2565fb19ed1f5147a1c27
Georgy Ushakov <ushakov@kodeworks.no>
2016-12-10 16:48:56 +0100

    Added rotation methods

--- furniture.js
+++ furniture.js
@@ -1,14 +1,14 @@
 let Furniture = {};
 
 Furniture.intersects = function(furniture1, furniture2){
-    var f1_x = furniture1.boundingBox[0];
-    var f1_dx = furniture1.boundingBox[1];
-    var f1_y = furniture1.boundingBox[2];
-    var f1_dy = furniture1.boundingBox[3];
-    var f2_x = furniture2.boundingBox[0];
-    var f2_dx = furniture2.boundingBox[1];
-    var f2_y = furniture2.boundingBox[2];
-    var f2_dy = furniture2.boundingBox[3];
+    const f1_x = furniture1.boundingBox[0];
+    const f1_dx = furniture1.boundingBox[1];
+    const f1_y = furniture1.boundingBox[2];
+    const f1_dy = furniture1.boundingBox[3];
+    const f2_x = furniture2.boundingBox[0];
+    const f2_dx = furniture2.boundingBox[1];
+    const f2_y = furniture2.boundingBox[2];
+    const f2_dy = furniture2.boundingBox[3];
     function is_inside(x, dx, y, dy, px, py) {
         if (x <= px && (x + dx) >= px &&
             y <= py && (y + dy) >= py)
@@ -28,6 +28,28 @@ Furniture.intersects = function(furniture1, furniture2){
 	return false;	
 };
 
+Furniture.flipBoundingBox = function(furniture1) {
+    const x = furniture1.boundingBox[0];
+    const dx = furniture1.boundingBox[1];
+    const y = furniture1.boundingBox[2];
+    const dy = furniture1.boundingBox[3];
+    furniture1.boundingBox[0] = -dy/2+(x+dx/2);
+    furniture1.boundingBox[1] = dy;
+    furniture1.boundingBox[2] = -dx/2+(y+dy/2);
+    furniture1.boundingBox[3] = dx;
+};
+
+Furniture.rotateClockwise = function(furniture1) {
+    Furniture.flipBoundingBox(furniture1);
+    furniture1.orientation += 90;
+    if (furniture1.orientation === 360) furniture1.orientation = 0;
+};
+
+Furniture.rotateCounterClockwise = function(furniture1) {
+    Furniture.flipBoundingBox(furniture1);
+    furniture1.orientation -= 90;
+    if (furniture1.orientation === -90) furniture1.orientation = 270;
+};
 
 Furniture.insideRoom = function(furniture1){
 	return false;	
@@ -63,5 +85,10 @@ $(document).ready(function() {
         boundingBox: [0, 10, 0, 10]
     };
     console.log("TEST a inside b: " + Furniture.intersects(f1, f2));
+    f1 = {
+        boundingBox: [2, 2, 4, 4]
+    };
+    Furniture.rotateClockwise(f1);
+    console.log("TEST rotate: " + f1.boundingBox);
 });
 
